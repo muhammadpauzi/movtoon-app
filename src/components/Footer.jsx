9bd9463664cb0e64fa5de3175dd2e0333f44f1a0
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-03-16 16:12:07 +0700

    fix: responsive of Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="py-10 border-t border-gray-200 px-5 text-center font-medium">
+        <footer className="py-5 md:py-10 border-t border-gray-200 px-2 text-center font-medium">
             <p>
                 Created with <span className="text-red-500">❤</span> by{' '}
                 <a href="" className="text-violet-600 font-bold">
