6c3eab83ab2dbe52f4f7b246ff3b641d14cb91db
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-03-16 15:28:10 +0700

    feat: create Tabs component

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,24 @@
+import { classNames } from '../utils';
+
+export default function Tabs({ tabs }) {
+    return (
+        <div className="flex items-center space-x-4 text-sm">
+            {tabs.map((tab, i) => {
+                return (
+                    <a
+                        key={i}
+                        href={tab.href}
+                        className={classNames(
+                            tab.active
+                                ? 'text-violet-600 border border-gray-200 bg-gray-100 p-2 px-3 rounded-md'
+                                : 'text-gray-700 transition hover:text-violet-600',
+                            'font-medium'
+                        )}
+                    >
+                        {tab.name}
+                    </a>
+                );
+            })}
+        </div>
+    );
+}
