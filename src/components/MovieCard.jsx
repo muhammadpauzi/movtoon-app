3d8fa2507c08a238174baa13abc7cd6ffee35d9a
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-03-16 16:17:31 +0700

    fix: responsive Movie Card component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,14 @@
 import movieImage from '../img/1.jpg';
-import { BookmarkIcon } from '@heroicons/react/outline';
 
 export default function MovieCard() {
     return (
         <a href="" className="group border-b border-gray-100">
-            <div className="overflow-hidden shadow-lg shadow-slate-200 mb-2">
-                <img src={movieImage} alt="" className="rounded" />
+            <div className="overflow-hidden shadow-lg shadow-slate-200">
+                <img src={movieImage} alt="" className="rounded w-full block" />
             </div>
 
-            <div className="p-2">
-                <h2 className="text-base text-gray-800 font-black mb-2 transition group-hover:text-violet-600">
+            <div className="p-2 py-3">
+                <h2 className="text-xs sm:text-base text-gray-800 font-black mb-2 transition group-hover:text-violet-600">
                     Where The Wild Things Are
                 </h2>
             </div>
